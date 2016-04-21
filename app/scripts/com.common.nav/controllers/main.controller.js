737662f205da08b69272c994540e6990431c5d16
杨灯 <yangdeng@haizhi.com>
2016-04-21 17:22:22 +0800

    1

--- app/scripts/com.common.nav/controllers/main.controller.js
+++ app/scripts/com.common.nav/controllers/main.controller.js
@@ -11,14 +11,32 @@
 
 (function () {
 
-	var commonNavMainCtrl = function ($timeout,commonFullpageConfig) {
+	var commonNavMainCtrl = function ($timeout, commonFullpageConfig, toaster) {
 
-		$timeout(function(){
+		var _self = this;
+
+		$timeout(function () {
 			$('#my_nav').fullpage(commonFullpageConfig.config);
 		});
+
+		/**
+		 * @description 右边导航toogle变化
+		 * @param e
+		 */
+		_self.navRToogle = function (e) {
+			e.stopPropagation();
+			_self.isShowNavR = (_self.isShowNavR) ? false : true;
+		};
+
+		_self.chooseNav = function (e) {
+			e.stopPropagation();
+			_self.isShowNavR = false;
+			toaster.warning('别着急','建设中.....')
+		}
+
 	};
 
-	commonNavMainCtrl.$inject = ['$timeout','commonFullpageConfig'];
+	commonNavMainCtrl.$inject = ['$timeout', 'commonFullpageConfig', 'toaster'];
 
 	angular.module('commonNavMain', [])
 		.controller('commonNavMainCtrl', commonNavMainCtrl);
